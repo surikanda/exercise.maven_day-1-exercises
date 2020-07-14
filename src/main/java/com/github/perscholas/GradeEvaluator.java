e9e81b54ec7810ffc77035a254821fad5745aa81
suresh kandagatla <surikandagatla@gmail.com>
2020-07-14 15:32:17 -0400

    Finished day1 exercises

--- src/main/java/com/github/perscholas/GradeEvaluator.java
+++ src/main/java/com/github/perscholas/GradeEvaluator.java
@@ -5,26 +5,47 @@ package com.github.perscholas;
  */
 public class GradeEvaluator {
     public Boolean isGradeA(Double score) {
-        return null;
+        if (score >= 90 && score <= 100){
+            return true;
+        }else return false;
     }
 
     public Boolean isGradeB(Double score) {
-        return null;
+        if(score >= 80 && score <= 89){
+            return true;
+        } else return false;
     }
 
     public Boolean isGradeC(Double score) {
-        return null;
+        if(score >= 70 && score <= 79){
+            return true;
+        } else return false;
     }
 
     public Boolean isGradeD(Double score) {
-        return null;
+        if(score >= 65 && score <= 69){
+            return true;
+        } else return false;
     }
 
     public Boolean isGradeF(Double score) {
-        return null;
+        if(score <=64){
+            return true;
+        } else return false;
     }
 
     public Character getGrade(Double score) {
-        return null;
+       // char grade = 'A';
+        if(isGradeA(score)){
+            return 'A';
+        } else if (isGradeB(score)){
+            return 'B';
+        } else if(isGradeC(score)){
+            return 'C';
+        } else if(isGradeD(score)){
+            return 'D';
+        } else if(isGradeF(score)){
+            return 'F';
+        } return 'N';
     }
 }
